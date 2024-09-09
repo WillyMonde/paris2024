900de56e8647c487223fb271338da39ad269e7c4
willy <monde.willy.pro@gmail.com>
2024-09-09 15:24:27 +0200

    Correction de la requête et correction des get puis changer le for en while

--- src/main/java/sio/paris2024/database/DaoPays.java
+++ src/main/java/sio/paris2024/database/DaoPays.java
@@ -5,12 +5,16 @@
 package sio.paris2024.database;
 
 import java.sql.Connection;
+import java.sql.Date;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ArrayList;
+import static sio.paris2024.database.DaoAthlete.requeteSql;
+import static sio.paris2024.database.DaoAthlete.resultatRequete;
 import static sio.paris2024.database.DaoPays.requeteSql;
 import static sio.paris2024.database.DaoPays.resultatRequete;
+import sio.paris2024.model.Athlete;
 import sio.paris2024.model.Pays;
 
 /**
@@ -49,5 +53,33 @@ public class DaoPays {
         return lesPays;
         
     }
-    
+        public static ArrayList<Athlete> getLesAthletesPaysById(Connection cnx, int idPays){
+        
+        ArrayList<Athlete>  lesAthletes = new ArrayList<>();
+        try{
+            requeteSql = cnx.prepareStatement("select a.id as a_id, a.nom as a_nom, a.prenom as a_prenom " +
+                         " from athlete a inner join pays p " +
+                         " on a.pays_id = p.id " + 
+                         " where p.id = ? ");
+            //System.out.println("REQ="+ requeteSql);
+            requeteSql.setInt(1, idPays);
+            resultatRequete = requeteSql.executeQuery();
+            
+            while (resultatRequete.next()){
+
+                   Athlete a = new Athlete();
+                   a.setId(resultatRequete.getInt("a_id"));
+                   a.setNom(resultatRequete.getString("a_nom"));
+                   a.setPrenom(resultatRequete.getString("a_prenom"));
+                   lesAthletes.add(a);
+                               
+            }
+           
+        }
+        catch (SQLException e){
+            e.printStackTrace();
+            System.out.println("La requête de getLesPompiers e généré une erreur");
+        }
+        return lesAthletes;
+    }
 }
