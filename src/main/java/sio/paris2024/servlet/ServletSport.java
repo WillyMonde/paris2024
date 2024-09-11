baeef14840be06ef32e28ea86c8f11911c5b758e
willy <monde.willy.pro@gmail.com>
2024-09-11 09:18:05 +0200

    création de la ServletSport

---
+++ src/main/java/sio/paris2024/servlet/ServletSport.java
@@ -0,0 +1,118 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/JSP_Servlet/Servlet.java to edit this template
+ */
+package sio.paris2024.servlet;
+
+import jakarta.servlet.ServletContext;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.util.ArrayList;
+import sio.paris2024.database.DaoSport;
+import sio.paris2024.model.Athlete;
+import sio.paris2024.model.Sport;
+
+/**
+ *
+ * @author SIO2
+ */
+@WebServlet(name = "ServletSport", urlPatterns = {"/ServletSport"})
+public class ServletSport extends HttpServlet {
+    
+    Connection cnx ;
+            
+    @Override
+    public void init()
+    {     
+        ServletContext servletContext=getServletContext();
+        
+        System.out.println("SERVLKET CONTEXT=" + servletContext.getContextPath());
+        cnx = (Connection)servletContext.getAttribute("connection"); 
+        
+        try {
+            System.out.println("INIT SERVLET=" + cnx.getSchema());
+        } catch (SQLException ex) {
+            Logger.getLogger(ServletSport.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+
+    /**
+     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
+     * methods.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+        try (PrintWriter out = response.getWriter()) {
+            /* TODO output your page here. You may use following sample code. */
+            out.println("<!DOCTYPE html>");
+            out.println("<html>");
+            out.println("<head>");
+            out.println("<title>Servlet ServletSport</title>");            
+            out.println("</head>");
+            out.println("<body>");
+            out.println("<h1>Servlet ServletSport at " + request.getContextPath() + "</h1>");
+            out.println("</body>");
+            out.println("</html>");
+        }
+    }
+
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        
+        String url = request.getRequestURI();  
+       
+        // Récup et affichage les sport
+        if(url.equals("/paris2024/ServletSport/lister"))
+        {              
+            ArrayList<Sport> lesSports = DaoSport.getLesSports(cnx);
+            request.setAttribute("sLesSports", lesSports);
+
+           getServletContext().getRequestDispatcher("/vues/sport/listerSport.jsp").forward(request, response);
+        }
+        
+        if(url.equals("/paris2024/ServletSport/consulter"))
+        { 
+            int idSport = Integer.parseInt((String)request.getParameter("idSport"));
+            ArrayList<Athlete> lesAthletes = DaoSport.getLesAthletesSportById(cnx, idSport);
+            request.setAttribute("sSport", lesAthletes);
+            
+           getServletContext().getRequestDispatcher("/vues/sport/consulterPays.jsp").forward(request, response);
+        }
+        
+        /*
+          if(url.equals("/paris2024/ServletSport/ajouter"))
+        {                   
+            ArrayList<Sport> lesSports = DaoSport.getLesSports(cnx);
+            request.setAttribute("sLesSports", lesSports);
+            this.getServletContext().getRequestDispatcher("/vues/sport/ajouterSport.jsp" ).forward( request, response );
+        }
+        */
+        
+    }
+      
+}
