c8818ccb881aa6d79b8409471cfc948dfd70a5ab
willy <monde.willy.pro@gmail.com>
2024-09-11 09:17:40 +0200

    création du model Sport

---
+++ src/main/java/sio/paris2024/model/Sport.java
@@ -0,0 +1,73 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package sio.paris2024.model;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author zakina
+ */
+public class Sport {
+    private int id;
+    private String nom ;
+    private ArrayList<Athlete> lesAthletes ;
+    private Athlete athlete;
+
+    public Sport() {
+    }
+
+    public Sport(int id, String nom, Athlete athlete) {
+        this.id = id;
+        this.nom = nom;
+        this.athlete = athlete;
+    }
+
+    public Sport(int id) {
+        this.id = id;
+    }
+    
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNom() {
+        return nom;
+    }
+
+    public void setNom(String nom) {
+        this.nom = nom;
+    }
+
+    public ArrayList<Athlete> getLesAthletes() {
+        return lesAthletes;
+    }
+
+    public void setLesAthletes(ArrayList<Athlete> lesAthletes) {
+        this.lesAthletes = lesAthletes;
+    }
+    
+    public void addAthlete(Athlete a){
+        
+        if (lesAthletes == null){
+            lesAthletes = new ArrayList<Athlete>();
+        }
+        lesAthletes.add(a);
+    }
+
+    public Athlete getAthlete() {
+        return athlete;
+    }
+
+    public void setAthlete(Athlete athlete) {
+        this.athlete = athlete;
+    }
+    
+    
+}
